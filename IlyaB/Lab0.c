1e49217bb2ba326cb9193d78d865d76444530a0c
Ilyablagov <101319139+Ilyablagov@users.noreply.github.com>
2025-09-23 22:37:09 +0300

    add_files

--- IlyaB/Lab0.c
+++ IlyaB/Lab0.c
@@ -1,1 +1,57 @@
-void main () {}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int max(float a, float b) {
+    if (a > b) return a;
+    else return b;
+}
+
+int min(float a, float b) {
+    if (a < b) return a;
+    else return b;
+}
+
+int main() {
+
+    float x1, y1, r1, x2, y2, r2, distance;
+
+    do {
+
+        printf("Input centre coordinates and two circles' radiuses");
+
+        scanf("%f%f%f%f%f%f", &x1, &y1, &r1, &x2, &y2, &r2);
+
+        if ((r1 == 0) || (r2 == 0)) printf("\033[41mRadius can't equal zero\033[49m\n");
+        else if ((r1 < 0) || (r2 < 0)) printf("\033[41mRadius can't subceed zero\033[49m\n");
+        printf("\n");
+
+    } while ((r1 <= 0) || (r2 <= 0));
+
+    distance = sqrt(pow(max(x1, x2) - min(x1, x2), 2) + pow(max(y1, y2) - min(y1, y2), 2));
+
+    if (distance == 0.0f) {
+
+        if (r1 == r2) printf("The circles are equal"); // 7
+        else if (r2 > r1) printf("The first circle is within the second one"); // 4
+        else printf("The second circle is within the first one"); // 4
+
+        printf("\nThe circles share a centre");
+
+    }
+    else {
+
+        if ((r1 + r2) == distance) printf("The circles contact each other");
+        else if (distance + min(r1, r2) == max(r1, r2)) printf("The circles contact each other\nOne circle is within the other one");
+        else if (distance + min(r1, r2) < max(r1, r2)) printf("One circle is within the other one");
+        else if (distance < (r1 + r2)) printf("The circles cross");
+        else printf("The circles don't contact each other");
+
+    }
+
+    printf("\r");
+
+    return 0;
+}
+
