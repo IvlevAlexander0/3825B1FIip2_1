17d749e63619c2444c43486b4eb7831fad63f2ef
unknown <this1smymail@yahoo.com>
2025-12-06 23:08:29 +0300

    Lab5

--- BespalovR/Lab№5/Lab№5.c
+++ BespalovR/Lab№5/Lab№5.c
@@ -1,20 +1,21 @@
-//по возрастанию 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <time.h>
+//На фоне 4 лабораторной, которая мне не зашла и на котору. ушло около 4 попыток, эта мне очень понравилась и я выложился по полной.
 struct row {
+	//Suggested that names are less than 16 in length.
 	char name[16];
-	unsigned short int age;
+	int age;
 };
 
-char choose_sort() {
-	printf("Choose sorting algorythm.\n");
+int choose_sort() {
+	printf("Choose sorting algorythm:\n");
 	printf("1 - Quick sort.\n");
 	printf("2 - Selection sort.\n");
 	printf("3 - Bubble sort.\n");
-	char t;
-	while (scanf("%d") != 1 || t > 3 || t < 1) {
+	int t = 0;
+	while (scanf("%d", &t) != 1 || t > 3 || t < 1) {
 		printf("Incorrect input. Please try again.\n");
 		char c;
 		while ( ( c = getchar()) != '\n' ){}
@@ -41,9 +42,9 @@ int partition(struct row* arr, int low, int high) {
 
 void quick_sort(struct row* arr, int low, int high) {
 	if (low < high) {
-		int pi = partition(arr, low, high);
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
+		int t = partition(arr, low, high);
+		quick_sort(arr, low, t - 1);
+		quick_sort(arr, t + 1, high);
 	}
 }
 
@@ -63,7 +64,7 @@ void selection_sort(struct row* arr, int n) {
 
 void bubble_sort(struct row* arr, int n) {
 	for (int i = 0; i < n - 1; ++i) {
-		char flag = 0;
+		int flag = 0;
 		for (int j = 0; j < n - i - 1; ++j) {
 			if (arr[j].age > arr[j + 1].age) {
 				struct row t = arr[j];
@@ -80,19 +81,77 @@ void bubble_sort(struct row* arr, int n) {
 }
 
 int read_file(struct row* arr) {
-	FILE* fptr = fopen("test.txt", "r+");
-	
+	FILE* fptr = fopen("People_in.txt", "r+");
+	if (fptr == NULL) {
+		return 0;
+	}
+	char c;
+	int n = 0;
+	//Suggested that file is formated the right way of "word number endline".
+	while (fscanf(fptr, "%s %d", &arr[n].name, &arr[n].age) == 2) {
+		n++;
+		if (n > 50) {
+			return -1;
+		}
+	}
 	fclose(fptr);
+	return n;
 }
 
 void write_file(struct row* arr, int n) {
-	FILE* fptr = fopen("test.txt", "w+");
+	FILE* fptr = fopen("People_out.txt", "w+");
+	//Suggested that fptr is not NULL and writing goes without errors.
 	for (int i = 0; i < n; ++i) {
 		fprintf(fptr, "%s\t%d\n", arr[i].name,arr[i].age);
 	}
 	fclose(fptr);
 }
 
-void main() {
+int main() {
 
+	struct row* arr = calloc(51, sizeof(struct row));
+	if (arr == NULL) {
+		printf("\nCould not allocate memory.\n");
+		return -1;
+	}
+
+	int n = read_file(arr);
+	if (n == 0) {
+		printf("\nCould not read file.\n");
+		return -1;
+	}
+	else if (n == -1) {
+		printf("Incorrect data format.\n");
+		return -1;
+	}
+
+	int option = choose_sort();
+	clock_t t = clock();
+	//Good place for function pointer but switch case will do the thing as well.
+	switch (option) {
+	case 1:
+		quick_sort(arr, 0, n-1);
+		break;
+	case 2:
+		selection_sort(arr, n);
+		break;
+	case 3:
+		bubble_sort(arr, n);
+		break;
+	}
+	t = clock() - t;
+	double time = ((double)t) / CLOCKS_PER_SEC;
+
+	write_file(arr,n);
+
+	printf("\n");
+	for (int i = 0; i < n; ++i) {
+		printf("%s\t%d\n", arr[i].name, arr[i].age);
+	}
+
+	printf("\nSorted data is stored in People_out.txt.\n");
+	printf("Sorting took %lf seconds\n", time);
+	//With my limited testing time is always 0 seconds. dunno whether or not it's supposed to be like that.
+	free(arr);
+	return 0;
 }
