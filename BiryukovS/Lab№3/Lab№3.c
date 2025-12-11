ea2d8ace3086f8ffddc7b8641f0719b17b6435d7
Besheniu Ezh <ssbiryukov65@gmail.com>
2025-12-11 23:42:43 +0300

    add_fies

--- BiryukovS/Lab№3/Lab№3.c
+++ BiryukovS/Lab№3/Lab№3.c
@@ -1,7 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define _CRT_SECURE_NO_WARNINGS
 
 int main() {
 	printf("Welcome to the most wondefull game \"BULLS AND COWS\"\n ");
