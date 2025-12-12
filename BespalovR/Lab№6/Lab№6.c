82604db2e93e9c5788b7dd7ce103bfd7b50094da
unknown <this1smymail@yahoo.com>
2025-12-13 02:44:36 +0300

    Lab№6_done

--- BespalovR/Lab№6/Lab№6.c
+++ BespalovR/Lab№6/Lab№6.c
@@ -57,7 +57,6 @@ long double lf_exp(long double eps, size_t n, long double x, size_t *i_count) {
 }
 long double lf_arccos(long double eps, size_t n, long double x, size_t *i_count) {
     if (x < -1 || x > 1) {
-        printf("Incorrect input. x must be in [-1;1].\n");
         return EOF;
     }
     long double res = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989/2;
@@ -134,7 +133,7 @@ int main() {
 
     int mode = select_mode();
     int choice = select_function();
-    size_t i;
+    size_t i = 0;
     size_t* i_count = &i;
     long double(*fnc)(long double) = standard_func(choice);
     long double(*lf_fnc)(long double, size_t, long double, size_t*) = lf_func(choice);
@@ -168,11 +167,15 @@ int main() {
             while ((c = getchar()) != '\n') {}
         }
 
-
-        printf("%Lf\n", fnc(x));
-        printf("%Lf\n", lf_fnc(eps, n, x, i_count));
-        printf("%Lf\n", fabsl(lf_fnc(eps, n, x, i_count) - fnc(x)));
-        printf("%zu\n", i);
+        if (lf_fnc(eps, n, x, i_count) != EOF) {
+            printf("%Lf\n", fnc(x));
+            printf("%Lf\n", lf_fnc(eps, n, x, i_count));
+            printf("%Lf\n", fabsl(lf_fnc(eps, n, x, i_count) - fnc(x)));
+            printf("%zu\n", i);
+        }
+        else {
+            printf("Incorrect input. x must be in [-1;1].\n");
+        }
         break;
     }
 
@@ -196,9 +199,14 @@ int main() {
             char c;
             while ((c = getchar()) != '\n') {}
         }
-
-        for (size_t n = 1; n < nmax + 1; ++n) {
-            printf("%zu\t%Lf\t%Lf\n", n, lf_fnc(0, n, x, i_count), fabsl(lf_fnc(0, n, x, i_count) - fnc(x)));
+        if (lf_fnc(0, 0, x, i_count) != EOF) {
+            for (size_t n = 1; n < nmax + 1; ++n) {
+                printf("%Lf\n", fnc(x));
+                printf("%zu\t%Lf\t%Lf\n", n, lf_fnc(0, n, x, i_count), fabsl(lf_fnc(0, n, x, i_count) - fnc(x)));
+            }
+        }
+        else {
+            printf("Incorrect input. x must be in [-1;1].\n");
         }
         break;
     }
