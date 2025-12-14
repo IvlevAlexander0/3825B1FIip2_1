f790e2c9cf76ac891c528b0db9ace8d9b03a4ac7
unknown <this1smymail@yahoo.com>
2025-12-14 13:16:38 +0300

    Lab№6_done_fix1

--- BespalovR/Lab№6/Lab№6.c
+++ BespalovR/Lab№6/Lab№6.c
@@ -50,8 +50,7 @@ long double lf_exp(long double eps, size_t n, long double x, size_t *i_count) {
         if (fabsl(term) < eps) {
             break;
         }
-        term *= x;
-        term /= ( i + 1);
+        term *= x/(i + 1);
     }
     return res;
 }
@@ -59,7 +58,7 @@ long double lf_arccos(long double eps, size_t n, long double x, size_t *i_count)
     if (x < -1 || x > 1) {
         return EOF;
     }
-    long double res = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989/2;
+    long double res = 3.1415926535897932/2;
     long double term = x;
     long double x_squared = x * x;
     *i_count = 0;
@@ -152,16 +151,16 @@ int main() {
 
         printf("Input EPS.(>=1e-16)\n");
         long double eps;
-        while (scanf("%Lf", &eps) != 1 || eps < 1e-16) {
+        while (scanf("%Lf", &eps) != 1 || eps < 1e-6) {
             printf("Incorrect input. Please try again.\n");
             char c;
             while ((c = getchar()) != '\n') {}
         }
 
 
-        printf("Input n.\n");
+        printf("Input n.(must be integer in [1;1000])\n");
         size_t n;
-        while (scanf("%zu", &n) != 1 ||  n < 1) {
+        while (scanf("%zu", &n) != 1 ||  n < 1 || n > 1000) {
             printf("Incorrect input. Please try again.\n");
             char c;
             while ((c = getchar()) != '\n') {}
@@ -192,9 +191,9 @@ int main() {
         }
 
 
-        printf("Input n.\n");
+        printf("Input nmax.(must be integer in [1;1000])\n");
         size_t nmax;
-        while (scanf("%zu", &nmax) != 1 || nmax < 1) {
+        while (scanf("%zu", &nmax) != 1 || nmax < 1 || nmax > 1000) {
             printf("Incorrect input. Please try again.\n");
             char c;
             while ((c = getchar()) != '\n') {}
